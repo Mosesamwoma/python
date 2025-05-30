c9d63e6e28775bff99be20cd640d33b08081d0bc
Moses <mosesamwoma@gmail.com>
2025-05-30 23:31:37 +0300

    factorial of a number

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("Hello")
+def factorial(n):
+    if n < 2:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+n = int(input("Enter a number: "))
+if n < 0:
+    print("undefined")
+else:
+    print("Factorial=", factorial(n))
